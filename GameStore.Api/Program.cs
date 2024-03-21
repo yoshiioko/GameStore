506cb57c73253826fde5503ac6f701cd87c775e7
yoshiioko <acardoza@pdx.edu>
2024-03-20 23:04:10 -0700

    Implement Post endpoint that returns the location of the created resource.

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,5 +1,7 @@
 using GameStore.Api.Entities;
 
+const string GetGameEndpointName = "GetGame";
+
 List<Game> games = new()
 {
     new Game()
@@ -46,6 +48,15 @@ app.MapGet("/games/{id}", (int id) =>
     }
 
     return Results.Ok(game);
+})
+.WithName(GetGameEndpointName);
+
+app.MapPost("/games", (Game game) =>
+{
+    game.Id = games.Max(game => game.Id) + 1;
+    games.Add(game);
+
+    return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
 });
 
 app.Run();
