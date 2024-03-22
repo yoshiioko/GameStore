85867a1e80a50b46397a30457c69dc8e285bb2d7
yoshiioko <acardoza@pdx.edu>
2024-03-21 19:57:28 -0700

    Refactor routes to use Route Group

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -36,9 +36,11 @@ List<Game> games = new()
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/games", () => games);
+var group = app.MapGroup("/games");
 
-app.MapGet("/games/{id}", (int id) =>
+group.MapGet("/", () => games);
+
+group.MapGet("/{id}", (int id) =>
 {
     Game? game = games.Find(game => game.Id == id);
 
@@ -51,7 +53,7 @@ app.MapGet("/games/{id}", (int id) =>
 })
 .WithName(GetGameEndpointName);
 
-app.MapPost("/games", (Game game) =>
+group.MapPost("/", (Game game) =>
 {
     game.Id = games.Max(game => game.Id) + 1;
     games.Add(game);
@@ -59,7 +61,7 @@ app.MapPost("/games", (Game game) =>
     return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
 });
 
-app.MapPut("/games/{id}", (int id, Game updatedGame) =>
+group.MapPut("/{id}", (int id, Game updatedGame) =>
 {
     Game? existingGame = games.Find(game => game.Id == id);
 
@@ -77,7 +79,7 @@ app.MapPut("/games/{id}", (int id, Game updatedGame) =>
     return Results.NoContent();
 });
 
-app.MapDelete("/games/{id}", (int id) =>
+group.MapDelete("/{id}", (int id) =>
 {
     Game? game = games.Find(game => game.Id == id);
 
