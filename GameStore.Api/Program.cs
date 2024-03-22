539225fc3510652ee3bf22c0612f67c00a6567f0
yoshiioko <acardoza@pdx.edu>
2024-03-22 10:47:50 -0700

    Register repository w/ app

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,10 @@
 using GameStore.Api.Endpoints;
+using GameStore.Api.Entities;
+using GameStore.Api.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton(typeof(IGamesRepository<Game, int>), typeof(InMemGamesRepository));
+
 var app = builder.Build();
 
 app.MapGamesEndpoints();
