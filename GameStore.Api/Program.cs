8689661c8e81c519442be8ea93e02294020484d3
yoshiioko <acardoza@pdx.edu>
2024-03-26 12:37:05 -0700

    Create policies for read & write with specified Scope and Role

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,3 +1,4 @@
+using GameStore.Api.Authorization;
 using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
@@ -5,7 +6,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRepositories(builder.Configuration);
 
 builder.Services.AddAuthentication().AddJwtBearer();
-builder.Services.AddAuthorization();
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy(Policies.ReadAccess, builder =>
+        builder.RequireClaim("scope", "games:read"));
+
+    options.AddPolicy(Policies.WriteAccess, builder =>
+        builder.RequireClaim("scope", "games:write")
+               .RequireRole("Admin"));
+});
 
 var app = builder.Build();
 
