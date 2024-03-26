8226834b191fea272ac5454a07cd9e1c01b246da
yoshiioko <acardoza@pdx.edu>
2024-03-26 13:30:09 -0700

    Use logger factory to create a logger

--- GameStore.Api/Data/DataExtensions.cs
+++ GameStore.Api/Data/DataExtensions.cs
@@ -11,6 +11,10 @@ public static class DataExtensions
         using var scope = serviceProvider.CreateScope();
         var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
         await dbContext.Database.MigrateAsync();
+
+        var logger = serviceProvider.GetRequiredService<ILoggerFactory>()
+                                    .CreateLogger("DB Initializer");
+        logger.LogInformation(5, "The database is ready!");
     }
 
     public static IServiceCollection AddRepositories(this IServiceCollection services, IConfiguration configuration)
