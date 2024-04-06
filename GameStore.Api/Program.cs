f15d926720865f04cdfe2159887f9833eb2a8f93
yoshiioko <acardoza@pdx.edu>
2024-04-06 13:07:28 -0700

    Add option to indent JSON logs for readibility

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -8,6 +8,14 @@ builder.Services.AddRepositories(builder.Configuration);
 builder.Services.AddAuthentication().AddJwtBearer();
 builder.Services.AddGameStoreAuthorization();
 
+builder.Logging.AddJsonConsole(options =>
+{
+    options.JsonWriterOptions = new()
+    {
+        Indented = true
+    };
+});
+
 var app = builder.Build();
 
 await app.Services.InitializeDbAsync();
