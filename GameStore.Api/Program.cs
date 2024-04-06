1668eb6af57c02355e68a884097354e7338a1526
yoshiioko <acardoza@pdx.edu>
2024-04-06 13:09:23 -0700

    Remove JSON logging format

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -8,14 +8,6 @@ builder.Services.AddRepositories(builder.Configuration);
 builder.Services.AddAuthentication().AddJwtBearer();
 builder.Services.AddGameStoreAuthorization();
 
-builder.Logging.AddJsonConsole(options =>
-{
-    options.JsonWriterOptions = new()
-    {
-        Indented = true
-    };
-});
-
 var app = builder.Build();
 
 await app.Services.InitializeDbAsync();
