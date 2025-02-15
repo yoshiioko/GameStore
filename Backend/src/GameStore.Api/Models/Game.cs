a0bda61ff28798e977df2e16392b0dfb16c13bb4
yoshiioko <acardoza@pdx.edu>
2025-02-14 21:35:28 -0800

    Add data annotations to Game model

--- Backend/src/GameStore.Api/Models/Game.cs
+++ Backend/src/GameStore.Api/Models/Game.cs
@@ -1,10 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameStore.Api.Models;
 
 public class Game
 {
     public Guid Id { get; set; }
+    
+    [Required]
+    [StringLength(50)]
     public required string Name { get; set; }
+    
+    [Required]
+    [StringLength(20)]
     public required string Genre { get; set; }
+    
+    [Range(1, 100)]
     public decimal Price { get; set; }
     public DateOnly ReleaseDate { get; set; }
 }
