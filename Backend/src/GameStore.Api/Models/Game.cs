40f1f7be377d76b6e8928a155f397a462f5b2efc
yoshiioko <acardoza@pdx.edu>
2025-02-15 13:03:03 -0800

    Remove data annotations from Model as not needed.

--- Backend/src/GameStore.Api/Models/Game.cs
+++ Backend/src/GameStore.Api/Models/Game.cs
@@ -1,20 +1,11 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace GameStore.Api.Models;
 
 public class Game
 {
     public Guid Id { get; set; }
-    
-    [Required]
-    [StringLength(50)]
     public required string Name { get; set; }
-    
     public required Genre Genre { get; set; }
-    
-    [Range(1, 100)]
     public decimal Price { get; set; }
-    
     public DateOnly ReleaseDate { get; set; }
     public required string Description { get; set; }
 }
