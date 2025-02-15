7fffa30ee84ae43c97ffa50280b78ef4001bdb61
yoshiioko <acardoza@pdx.edu>
2025-02-14 20:15:15 -0800

    Implement GET All endpoint. Create sample in-mem data

--- Backend/src/GameStore.Api/Program.cs
+++ Backend/src/GameStore.Api/Program.cs
@@ -1,6 +1,36 @@
+using GameStore.Api.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<Game> games =
+[
+    new ()
+    {
+        Id = Guid.NewGuid(),
+        Name = "Street Fighter II",
+        Genre = "Fighting",
+        Price = 19.99m,
+        ReleaseDate = new DateOnly(1992, 7, 15)
+    },
+    new()
+    {
+        Id = Guid.NewGuid(),
+        Name = "Final Fantasy XIV",
+        Genre = "Roleplaying",
+        Price = 59.99m,
+        ReleaseDate = new DateOnly(2010, 9, 30)
+    },
+    new()
+    {
+        Id = Guid.NewGuid(),
+        Name = "FIFA 23",
+        Genre = "Sports",
+        Price = 69.99m,
+        ReleaseDate = new DateOnly(2022, 9, 27)
+    }
+];
+
+app.MapGet("/games", () => games);
 
 app.Run();
