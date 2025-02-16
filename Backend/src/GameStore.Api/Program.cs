05df67230ad54f056c8171acdbd6003e300197d3
yoshiioko <acardoza@pdx.edu>
2025-02-15 21:39:56 -0800

    Update endpoints to use GameStoreData class

--- Backend/src/GameStore.Api/Program.cs
+++ Backend/src/GameStore.Api/Program.cs
@@ -1,66 +1,43 @@
 using GameStore.Api.Models;
 using System.ComponentModel.DataAnnotations;
+using GameStore.Api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 const string GetGameEndpointName = "GetGame";
 
-List<Genre> genres = 
-[
-    new() {Id = new Guid("1319fe82-459d-4eaa-b5fb-0f67357f9037"), Name = "Fighting"}, 
-    new() {Id = new Guid("fc44dbe1-198b-4b8f-9a3f-8c5ba556b1e1"), Name = "Kids and Family"}, 
-    new() {Id = new Guid("270d8959-4445-4996-99ef-f2a6d9346d35"), Name = "Racing"}, 
-    new() {Id = new Guid("c7d2dd03-ddfe-4557-84a1-62f0eaf2c105"), Name = "Roleplaying"}, 
-    new() {Id = new Guid("fc0237d0-0330-4899-978b-6d381493932e"), Name = "Sports"},
-];
+var data = new GameStoreData();
 
-List<Game> games =
-[
-    new()
-    {
-        Id = Guid.NewGuid(),
-        Name = "Street Fighter II",
-        Genre = genres[0],
-        Price = 19.99m,
-        ReleaseDate = new DateOnly(1992, 7, 15),
-        Description = "Street Fighter II is regarded as one of the greatest video games of all time and the most important and influential fighting game ever made."
-    },
-    new()
-    {
-        Id = Guid.NewGuid(),
-        Name = "Final Fantasy XIV",
-        Genre = genres[3],
-        Price = 59.99m,
-        ReleaseDate = new DateOnly(2010, 9, 30),
-        Description = "Final Fantasy XIV is a massively multiplayer online role-playing game (MMORPG) developed and published by Square Enix."
-    },
-    new()
-    {
-        Id = Guid.NewGuid(),
-        Name = "FIFA 23",
-        Genre = genres[4],
-        Price = 69.99m,
-        ReleaseDate = new DateOnly(2022, 9, 27),
-        Description = "FIFA 23 is a football video game published by EA Sports."
-    }
-];
-
-app.MapGet("/games", () => games.Select(game => new GameSummaryDto(
-    game.Id, game.Name, game.Genre.Name, game.Price, game.ReleaseDate)));
+app.MapGet("/games", () =>
+{
+    return data.GetGames()
+               .Select(game => new GameSummaryDto(
+                   game.Id, 
+                   game.Name, 
+                   game.Genre.Name, 
+                   game.Price, 
+                   game.ReleaseDate));
+});
 
 app.MapGet("/games/{id:guid}", (Guid id) =>
 {
-    var game = games.Find(game => game.Id == id);
+    var game = data.GetGame(id);
 
     return game is null ? Results.NotFound() : Results.Ok(
-        new GameDetailsDto(game.Id, game.Name, game.Genre.Id, game.Price, game.ReleaseDate, game.Description));
+        new GameDetailsDto(
+            game.Id, 
+            game.Name, 
+            game.Genre.Id, 
+            game.Price, 
+            game.ReleaseDate, 
+            game.Description));
 })
 .WithName(GetGameEndpointName);
 
 app.MapPost("/games", (CreateGameDto gameDto) =>
 {
-    var genre = genres.Find(genre => genre.Id == gameDto.GenreId);
+    var genre = data.GetGenre(gameDto.GenreId);
 
     if (genre is null)
     {
@@ -69,7 +46,6 @@ app.MapPost("/games", (CreateGameDto gameDto) =>
 
     var game = new Game
     {
-        Id = Guid.NewGuid(),
         Name = gameDto.Name,
         Genre = genre,
         Price = gameDto.Price,
@@ -77,8 +53,8 @@ app.MapPost("/games", (CreateGameDto gameDto) =>
         Description = gameDto.Description
     };
     
-    games.Add(game);
-
+    data.AddGame(game);
+    
     var gamesResponse = new GameDetailsDto(
         game.Id,
         game.Name,
@@ -94,14 +70,14 @@ app.MapPost("/games", (CreateGameDto gameDto) =>
 
 app.MapPut("/games/{id:guid}", (Guid id, UpdateGameDto gameDto) =>
 {
-    var existingGame = games.Find(game => game.Id == id);
+    var existingGame = data.GetGame(id);
 
     if (existingGame is null)
     {
         return Results.NotFound();
     }
     
-    var genre = genres.Find(genre => genre.Id == gameDto.GenreId);
+    var genre = data.GetGenre(gameDto.GenreId);
 
     if (genre is null)
     {
@@ -114,20 +90,21 @@ app.MapPut("/games/{id:guid}", (Guid id, UpdateGameDto gameDto) =>
     existingGame.ReleaseDate = gameDto.ReleaseDate;
     existingGame.Description = gameDto.Description;
 
-    return Results.NoContent();
+    return Results.NoContent(); 
 })
 .WithParameterValidation();
 
 app.MapDelete("/games/{id:guid}", (Guid id) =>
 {
-    games.RemoveAll(game => game.Id == id);
+    data.RemoveGame(id);
 
     return Results.NoContent();
 });
 
 app.MapGet("/genres", () =>
 {
-    return genres.Select(genre => new GenreDto(genre.Id, genre.Name));
+    return data.GetGenres()
+               .Select(genre => new GenreDto(genre.Id, genre.Name));
 });
 
 app.Run();
