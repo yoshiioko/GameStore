30bd8c6b08377a9227907367cfce73e856f8f3b2
yoshiioko <acardoza@pdx.edu>
2025-02-16 13:27:00 -0800

    Run formatter.

--- Backend/src/GameStore.Api/Data/GameStoreData.cs
+++ Backend/src/GameStore.Api/Data/GameStoreData.cs
@@ -4,20 +4,20 @@ namespace GameStore.Api.Data;
 
 public class GameStoreData
 {
-    private readonly List<Genre> _genres = 
+    private readonly List<Genre> _genres =
     [
-        new() {Id = new Guid("1319fe82-459d-4eaa-b5fb-0f67357f9037"), Name = "Fighting"}, 
-        new() {Id = new Guid("fc44dbe1-198b-4b8f-9a3f-8c5ba556b1e1"), Name = "Kids and Family"}, 
-        new() {Id = new Guid("270d8959-4445-4996-99ef-f2a6d9346d35"), Name = "Racing"}, 
-        new() {Id = new Guid("c7d2dd03-ddfe-4557-84a1-62f0eaf2c105"), Name = "Roleplaying"}, 
-        new() {Id = new Guid("fc0237d0-0330-4899-978b-6d381493932e"), Name = "Sports"},
+        new() { Id = new Guid("1319fe82-459d-4eaa-b5fb-0f67357f9037"), Name = "Fighting" },
+        new() { Id = new Guid("fc44dbe1-198b-4b8f-9a3f-8c5ba556b1e1"), Name = "Kids and Family" },
+        new() { Id = new Guid("270d8959-4445-4996-99ef-f2a6d9346d35"), Name = "Racing" },
+        new() { Id = new Guid("c7d2dd03-ddfe-4557-84a1-62f0eaf2c105"), Name = "Roleplaying" },
+        new() { Id = new Guid("fc0237d0-0330-4899-978b-6d381493932e"), Name = "Sports" },
     ];
 
     private readonly List<Game> _games;
 
     public GameStoreData()
     {
-        _games = 
+        _games =
         [
             new Game
             {
@@ -26,7 +26,8 @@ public class GameStoreData
                 Genre = _genres[0],
                 Price = 19.99m,
                 ReleaseDate = new DateOnly(1992, 7, 15),
-                Description = "Street Fighter II is regarded as one of the greatest video games of all time and the most important and influential fighting game ever made."
+                Description =
+                    "Street Fighter II is regarded as one of the greatest video games of all time and the most important and influential fighting game ever made."
             },
             new Game
             {
@@ -35,7 +36,8 @@ public class GameStoreData
                 Genre = _genres[3],
                 Price = 59.99m,
                 ReleaseDate = new DateOnly(2010, 9, 30),
-                Description = "Final Fantasy XIV is a massively multiplayer online role-playing game (MMORPG) developed and published by Square Enix."
+                Description =
+                    "Final Fantasy XIV is a massively multiplayer online role-playing game (MMORPG) developed and published by Square Enix."
             },
             new Game
             {
