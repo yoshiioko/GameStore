64b5fea31cd7c74850651d28bcfea90639663903
yoshiioko <acardoza@pdx.edu>
2025-02-17 21:17:16 -0800

    Create DbContext class and install EF

---
+++ Backend/src/GameStore.Api/Data/GameStoreContext.cs
@@ -0,0 +1,11 @@
+using GameStore.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Api.Data;
+
+public class GameStoreContext(DbContextOptions<GameStoreContext> options) : DbContext(options)
+{
+    public DbSet<Game> Games => Set<Game>();
+
+    public DbSet<Genre> Genres => Set<Genre>();
+}
