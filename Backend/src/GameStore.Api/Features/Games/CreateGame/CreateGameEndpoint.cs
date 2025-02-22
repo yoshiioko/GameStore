585fcb08305a9b3c440bad5dc2b56697df01d758
yoshiioko <acardoza@pdx.edu>
2025-02-21 22:09:11 -0800

    Update POST Games endpoint to use EF Core.

--- Backend/src/GameStore.Api/Features/Games/CreateGame/CreateGameEndpoint.cs
+++ Backend/src/GameStore.Api/Features/Games/CreateGame/CreateGameEndpoint.cs
@@ -8,32 +8,24 @@ public static class CreateGameEndpoint
 {
     public static void MapCreateGame(this IEndpointRouteBuilder app)
     {
-        app.MapPost("/", (CreateGameDto gameDto, GameStoreData data, GameDataLogger logger) =>
+        app.MapPost("/", (CreateGameDto gameDto, GameStoreContext dbContext) =>
             {
-                var genre = data.GetGenre(gameDto.GenreId);
-
-                if (genre is null)
-                {
-                    return Results.BadRequest("Invalid Genre Id");
-                }
-
                 var game = new Game
                 {
                     Name = gameDto.Name,
-                    Genre = genre,
-                    GenreId = genre.Id,
+                    GenreId = gameDto.GenreId,
                     Price = gameDto.Price,
                     ReleaseDate = gameDto.ReleaseDate,
                     Description = gameDto.Description
                 };
 
-                data.AddGame(game);
-                logger.PrintGames();
+                dbContext.Games.Add(game);
+                dbContext.SaveChanges();
 
                 var gamesResponse = new GameDetailsDto(
                     game.Id,
                     game.Name,
-                    game.Genre.Id,
+                    game.GenreId,
                     game.Price,
                     game.ReleaseDate,
                     game.Description
