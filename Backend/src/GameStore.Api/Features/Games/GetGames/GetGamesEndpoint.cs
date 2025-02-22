fd1002973a032fdfba861f26990dbd03fde1e44a
yoshiioko <acardoza@pdx.edu>
2025-02-21 22:35:31 -0800

    Update GET Games endpoint to use EF Core.

--- Backend/src/GameStore.Api/Features/Games/GetGames/GetGamesEndpoint.cs
+++ Backend/src/GameStore.Api/Features/Games/GetGames/GetGamesEndpoint.cs
@@ -1,4 +1,5 @@
 using GameStore.Api.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Features.Games.GetGames;
 
@@ -6,15 +7,17 @@ public static class GetGamesEndpoint
 {
     public static void MapGetGames(this IEndpointRouteBuilder app)
     {
-        app.MapGet("/", (GameStoreData data) =>
+        app.MapGet("/", (GameStoreContext dbContext) =>
         {
-            return data.GetGames()
+            return dbContext.Games
+                .Include(game => game.Genre)
                 .Select(game => new GameSummaryDto(
                     game.Id,
                     game.Name,
                     game.Genre!.Name,
                     game.Price,
-                    game.ReleaseDate));
+                    game.ReleaseDate))
+                .AsNoTracking();
         });
     }
 }
