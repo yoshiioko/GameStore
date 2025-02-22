9f61eee5ce83da79fbfd5ec522344ce5db17fe88
yoshiioko <acardoza@pdx.edu>
2025-02-21 22:01:35 -0800

    Implementing extension method to seed database and to initialize it.

--- Backend/src/GameStore.Api/Data/DataExtensions.cs
+++ Backend/src/GameStore.Api/Data/DataExtensions.cs
@@ -1,14 +1,41 @@
+using GameStore.Api.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Data;
 
 public static class DataExtensions
 {
+    public static void InitializeDb(this WebApplication app)
+    {
+        app.MigrateDb();
+        app.SeedDb();
+    }
+
     public static void MigrateDb(this WebApplication app)
     {
         // Manually provision a scope for DbContext
         using var scope = app.Services.CreateScope();
         var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
         dbContext.Database.Migrate();
+    }
+
+    public static void SeedDb(this WebApplication app)
+    {
+        // Manually provision a scope for DbContext
+        using var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
+
+        if (!dbContext.Genres.Any())
+        {
+            dbContext.Genres.AddRange(
+                new Genre { Name = "Fighting" },
+                new Genre { Name = "Kids and Family" },
+                new Genre { Name = "Racing" },
+                new Genre { Name = "Roleplaying" },
+                new Genre { Name = "Sports" }
+            );
+
+            dbContext.SaveChanges();
+        }
     }
 }
--- Backend/src/GameStore.Api/Program.cs
+++ Backend/src/GameStore.Api/Program.cs
@@ -14,6 +14,6 @@ var app = builder.Build();
 
 app.MapGames();
 app.MapGenres();
-app.MigrateDb();
+app.InitializeDb();
 
 app.Run();
