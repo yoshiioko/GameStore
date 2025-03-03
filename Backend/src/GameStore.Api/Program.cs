caa50008e0d0464fab74f5a2359c0146ab3e2a7b
yoshiioko <acardoza@pdx.edu>
2025-03-02 21:54:58 -0800

    Use/Configure app to use HttpLogging Middleware. Disable custom logging middleware.

--- Backend/src/GameStore.Api/Program.cs
+++ Backend/src/GameStore.Api/Program.cs
@@ -1,7 +1,7 @@
 using GameStore.Api.Data;
 using GameStore.Api.Features.Games;
 using GameStore.Api.Features.Genres;
-using GameStore.Api.Shared.Timing;
+using Microsoft.AspNetCore.HttpLogging;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,12 +9,19 @@ var connString = builder.Configuration.GetConnectionString("GameStore");
 
 builder.Services.AddSqlite<GameStoreContext>(connString);
 
+builder.Services.AddHttpLogging(options =>
+{
+    options.LoggingFields = HttpLoggingFields.RequestMethod | HttpLoggingFields.RequestPath |
+                            HttpLoggingFields.ResponseStatusCode | HttpLoggingFields.Duration;
+    options.CombineLogs = true;
+});
+
 var app = builder.Build();
 
 app.MapGames();
 app.MapGenres();
 
-app.UseMiddleware<RequestTimingMiddleware>();
+app.UseHttpLogging();
 
 await app.InitializeDbAsync();
 
