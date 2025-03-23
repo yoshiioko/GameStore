d37973edd5ed102420ca850b3861b9269aa12255
yoshiioko <acardoza@pdx.edu>
2025-03-22 20:57:30 -0700

    Add DTOs for pagination

--- Backend/src/GameStore.Api/Features/Games/GetGames/GetGamesDtos.cs
+++ Backend/src/GameStore.Api/Features/Games/GetGames/GetGamesDtos.cs
@@ -1,5 +1,9 @@
 namespace GameStore.Api.Features.Games.GetGames;
 
+public record GetGamesDto(int PageNumber = 1, int PageSize = 5);
+
+public record GamesPageDto(int TotalPages, IEnumerable<GameSummaryDto> Data);
+
 public record GameSummaryDto(
     Guid Id,
     string Name,
