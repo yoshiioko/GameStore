4efcc007c46faf8d4555e75a2e741a55ac768603
yoshiioko <acardoza@pdx.edu>
2025-03-22 21:21:24 -0700

    Implement pagination on GET Games endpoint.

--- Backend/src/GameStore.Api/Features/Games/GetGames/GetGamesEndpoint.cs
+++ Backend/src/GameStore.Api/Features/Games/GetGames/GetGamesEndpoint.cs
@@ -7,9 +7,15 @@ public static class GetGamesEndpoint
 {
     public static void MapGetGames(this IEndpointRouteBuilder app)
     {
-        app.MapGet("/", async (GameStoreContext dbContext) =>
+        app.MapGet("/", async (GameStoreContext dbContext, [AsParameters] GetGamesDto request) =>
         {
-            return await dbContext.Games
+            // Simple logic used for pagination purposes
+            var skipCount = (request.PageNumber - 1) * request.PageSize;
+
+            var gamesOnPage = await dbContext.Games
+                .OrderBy(game => game.Name)
+                .Skip(skipCount)
+                .Take(request.PageSize)
                 .Include(game => game.Genre)
                 .Select(game => new GameSummaryDto(
                     game.Id,
@@ -19,6 +25,11 @@ public static class GetGamesEndpoint
                     game.ReleaseDate))
                 .AsNoTracking()
                 .ToListAsync();
+
+            var totalGames = await dbContext.Games.CountAsync();
+            var totalPages = (int)Math.Ceiling(totalGames / (double)request.PageSize);
+
+            return new GamesPageDto(totalPages, gamesOnPage);
         });
     }
 }
